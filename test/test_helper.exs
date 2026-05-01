59369a48ad17ed87f0961df4fab73221fd0ea9eb
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-05-01 17:50:03 +0200

    Add sanity check for JSON validity

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,7 +3,11 @@ ExUnit.start()
 defmodule SchemaIrveTest do
   use ExUnit.Case
 
-  test "sanity check" do
-    assert 1 == 1
+  test "statique JSON is valid" do
+    assert {:ok, _} = "statique/schema-statique.json" |> File.read!() |> JSON.decode()
+  end
+
+  test "dynamique JSON is valid" do
+    assert {:ok, _} = "dynamique/schema-dynamique.json" |> File.read!() |> JSON.decode()
   end
 end
