828b0070e6fc32a758c4f658ac6f1851540e5c05
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-05-02 00:17:56 +0200

    Add tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -10,4 +10,16 @@ defmodule SchemaIrveTest do
   test "dynamique JSON is valid" do
     assert {:ok, _} = "dynamique/schema-dynamique.json" |> File.read!() |> JSON.decode()
   end
+
+  test "statique-tarifs JSON is valid" do
+    assert {:ok, _} = "statique-tarifs/schema-statique-tarifs.json" |> File.read!() |> JSON.decode()
+  end
+
+  test "restrictions JSON Schema is valid" do
+    assert {:ok, _} = "statique-tarifs/restrictions.schema.json" |> File.read!() |> JSON.decode()
+  end
+
+  test "price-components JSON Schema is valid" do
+    assert {:ok, _} = "statique-tarifs/price-components.schema.json" |> File.read!() |> JSON.decode()
+  end
 end
